9e0766ab322f07bb30820771881e639f902e1122
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2024-11-23 15:01:27 -0300

    teste

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -9,7 +9,28 @@ import { Component, Inject } from '@angular/core';
 export class FirstComponentComponent {
   contador = 0;
 
+  handleSocketError(error: any) {
+    console.error('Erro no websocket:', error);
+  }
+
+  handleSocketClose() {
+    console.log('Websocket fechado.');
+  }
+
+  handleSocketOpen() {
+    console.log('Websocket conectado.');
+  }
+
   quandoClicado() {
+    // const socket: WebSocket = new WebSocket('ws://localhost:8080/teste');
+    // socket.addEventListener('open', this.handleSocketOpen);
+    // socket.addEventListener('error', this.handleSocketError);
+    // socket.addEventListener('close', this.handleSocketClose);
+    // socket.onmessage = (event) => {
+    //   console.log(event.data);
+    //   this.contador += event.data;
+    // };
+
     this.contador += 1;
   }
 }
