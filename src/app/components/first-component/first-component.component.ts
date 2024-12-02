ebbd33c5d39d6ae355191ef6d8f3fb842b0b722b
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2024-12-02 16:47:20 -0300

    odeio front

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
@@ -7,42 +7,49 @@ import { Component, Inject } from '@angular/core';
   styleUrl: './first-component.component.css',
 })
 export class FirstComponentComponent {
-  contador = 'alguma mensagem';
+  contador = 'Esta é uma mensagem enviada pelo 4200';
 
-  handleSocketError(error: any) {
-    console.error('Erro no websocket:', error);
-  }
+  // handleSocketError(error: any) {
+  //   console.error('Erro no websocket:', error);
+  // }
 
-  handleSocketClose() {
-    console.log('Websocket fechado.');
-  }
+  // handleSocketClose() {
+  //   console.log('Websocket fechado.');
+  // }
 
-  handleSocketOpen(socket: WebSocket) {
-    console.log('Websocket conectado.');
-    socket.send(this.contador);
+  // handleSocketOpen(socket: WebSocket) {
+  //   console.log('Websocket conectado.');
+  //   socket.send(this.contador);
+  // }
+
+  digitou() {
+    console.log('pera ai');
   }
 
   quandoClicado() {
-    let socket = new WebSocket('ws://localhost:8080/teste');
-    socket.onopen = (event) => {
-      this.handleSocketOpen(socket);
-    };
-    // socket.addEventListener('open', this.handleSocketOpen);
-    socket.onerror = (error) => {
-      this.handleSocketError(error);
-    };
-    // socket.addEventListener('error', this.handleSocketError);
-    socket.onclose = (event) => {
-      this.handleSocketClose();
-    };
-    // socket.addEventListener('close', this.handleSocketClose);
-    socket.onmessage = (event) => {
-      console.log(event.data);
-      this.contador += event.data;
-    };
-
+    // let socket = new WebSocket('ws://localhost:8080/teste');
+    // socket.onopen = (event) => {
+    //   this.handleSocketOpen(socket);
+    // };
+    // // socket.addEventListener('open', this.handleSocketOpen);
+    // socket.onerror = (error) => {
+    //   this.handleSocketError(error);
+    // };
+    // // socket.addEventListener('error', this.handleSocketError);
+    // socket.onclose = (event) => {
+    //   this.handleSocketClose();
+    // };
+    // // socket.addEventListener('close', this.handleSocketClose);
+    // socket.onmessage = (event) => {
+    //   if (event.data === 'Esta é uma mensagem enviada pelo 4200') {
+    //     console.log(
+    //       'A função funciona também quando enviamos a mensagem e não apenas quando recebemos'
+    //     );
+    //   }
+    //   console.log(event.data);
+    //   document.body.innerHTML += `${event.data}`
+    // };
     // this.contador = '';
-
     // this.contador += 1;
   }
 }
